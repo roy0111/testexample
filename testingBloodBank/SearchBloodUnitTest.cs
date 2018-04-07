30802087876b749bc1f9d3699dbb6c13ff48ed32
roy0111 <biratewu0111@gmail.com>
2018-04-07 23:42:54 +0600

    Add files via upload

--- testingBloodBank/SearchBloodUnitTest.cs
+++ testingBloodBank/SearchBloodUnitTest.cs
@@ -15,24 +15,44 @@ namespace testingBloodBank
         [Test()]//test case 1 
         public void CheckNullTestCase()
         {
-            string dropDownFieldText = "123456789";
+            string dropDownFieldText = null;
 
             bool result = obj.CheckFieldValidity(dropDownFieldText);
-            bool expectedResult = true;
-            Assert.AreEqual(expectedResult, result);
-        }
-
-        [Test()]//test case 2
-        public void SearchDonorByBlood()
-        {
-            string requiredBloodGroup = "k+";
-            string requiredArea = "Badda";
-
-            bool result = obj.CheckBloodAvaiability(requiredBloodGroup, requiredArea);
             bool expectedResult = false;
             Assert.AreEqual(expectedResult, result);
         }
 
 
+
+        [Test()]//test case 3
+        public void SearchAvailableDonorByBlood()
+        {
+            string requiredBloodGroup = "ab+";
+
+            bool result = obj.CheckBloodGroupDonorAvailableOrNot(requiredBloodGroup);
+            bool expectedResult = true;
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test()]//test case 4
+        public void CheckAreaAvailableOrNot()
+        {
+            string areaName = "Badda";
+
+            bool result = obj.CheckAreaAvailableOrNot(areaName);
+            bool expectedResult = true;
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test()]//test case 2
+        public void SearchDonorByBloodInArea()
+        {
+            string requiredBloodGroup = "b+";
+            string requiredArea = "Badda";
+
+            bool result = obj.CheckBloodAvailableInParticulerAreaOrNot(requiredBloodGroup, requiredArea);
+            bool expectedResult = true;
+            Assert.AreEqual(expectedResult, result);
+        }
     }
 }
